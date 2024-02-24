f4fcba6d3b6b2ee726f437720904f3eb2f51b46c
Tristan Ford <tristan4d@gmail.com>
2024-02-23 17:04:59 -0800

    mappedin test, not working yet

--- app/(tabs)/garage.tsx
+++ app/(tabs)/garage.tsx
@@ -33,34 +33,6 @@ export default function garage() {
   const [floor, setFloor] = useState(0);
 
   return (
-    // <View className="bg-offWhite flex flex-col h-full">
-    //   <View className="basis-1/12 flex flex-row justify-evenly items-center">
-    //     <View className="basis-1/3 border-solid border-2 border-purple rounded overflow-hidden">
-    //       <Text className="text-purple font-semibold p-1 text-center">
-    //         Garage
-    //       </Text>
-    //     </View>
-    //     <View className="basis-1/4">
-    //       <Dropdown
-    //         selectedTextStyle={{
-    //           color: "#5350B7",
-    //           fontWeight: "600",
-    //           textAlign: "center",
-    //         }}
-    //         data={data}
-    //         labelField="label"
-    //         valueField="value"
-    //         searchPlaceholder="Search..."
-    //         value={floor}
-    //         onChange={(item) => {
-    //           setFloor(item.value);
-    //         }}
-    //       />
-    //     </View>
-    //   </View>
-    //   <Separator />
-    //   <GarageFloor spaces={spaces[floor]} />
-    // </View>
     <GarageMap />
   );
 }
