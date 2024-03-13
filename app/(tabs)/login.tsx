f76ec0716beab83d56f99215b9411767f1b3b1b7
Cameron Leong <cameronleong531@gmail.com>
2024-03-12 19:37:39 -0700

    login system now detects existing user logins as well as new user registration. styling still in progress.

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
 import { View, TextInput, Text, Button } from "react-native";
 import { auth } from "../../firebase/firebase"
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 
 export default function Login() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
+  const handleRegister = () => {
+    createUserWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        console.log(userCredential)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  };
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        console.log(userCredential)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  };
+
   return (
     <View className="flex-1 justify-center items-center">
-      <TextInput className="text-black" onChangeText={setUsername} value={username} placeholder="Username"/>
-      <TextInput onChangeText={setPassword} value={password} placeholder="Password"/>
-      <Button title="submit" onPress={()=>{
-        createUserWithEmailAndPassword(auth, username, password).then((userCredential)=>{
-            console.log(userCredential)
-        }).catch((err)=>{
-            console.log(err)
-        })
-      }}/>
+      <TextInput className="text-black bg-gray-200 border border-gray-300 p-2 mt-2 mb-2 rounded w-4/5" 
+      onChangeText={setUsername} value={username} placeholder="Username"/>
+      <TextInput className="text-black bg-gray-200 border border-gray-300 p-2 mt-2 mb-2 rounded w-4/5"
+      onChangeText={setPassword} value={password} placeholder="Password"/>
+      
+      <Button title="Register" onPress={handleRegister} />
+      <Button title="Login" onPress={handleLogin} />
+
     </View>
   );
 }
