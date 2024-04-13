13b51bd2175527f6b2e5c6634d3f7cc58d38eb39
Jerry Sun <sunyiran0817@gmail.com>
2024-04-12 21:15:19 -0700

    completed app

---
+++ components/dropdown.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+const data = [
+  { label: "Floor 1", value: "1" },
+  { label: "Floor 2", value: "2" },
+  { label: "Floor 3", value: "3" },
+  { label: "Floor 4", value: "4" },
+  { label: "Floor 5", value: "5" },
+];
+
+const DropdownComponent = ({ selectedFloor, onFloorChange }) => {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? "Select floor" : "..."}
+        searchPlaceholder="Search..."
+        value={selectedFloor}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          onFloorChange(item.value);
+          setIsFocus(false);
+        }}
+        renderLeftIcon={() => (
+          <AntDesign
+            style={styles.icon}
+            color={isFocus ? "blue" : "black"}
+            name="Safety"
+            size={20}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+export default DropdownComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 8,
+    flex: 1,
+    zIndex: 1000,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    flex: 1,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
---
+++ components/parkingGrid.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import carIcon from "../assets/icons/car.png";
+import disabilityIcon from "../assets/icons/disability.png";
+
+const parkingSpots = {
+  "1": [
+    // Left side
+    { id: "A-01", occupied: false, isDisable: true, type: "car" },
+    { id: "A-02", occupied: false, isDisable: true, type: "car" },
+    { id: "A-03", occupied: true, isDisable: false, type: "car" },
+    { id: "A-04", occupied: true, isDisable: false, type: "car" },
+    { id: "A-05", occupied: true, isDisable: false, type: "car" },
+    { id: "A-6", occupied: false, isDisable: false, type: "car" },
+
+    // Right side, after left side spots
+    { id: "A-07", occupied: false, isDisable: true, type: "car" },
+    { id: "A-08", occupied: false, isDisable: true, type: "car" },
+    { id: "A-09", occupied: true, isDisable: false, type: "car" },
+    { id: "A-10", occupied: true, isDisable: false, type: "car" },
+    { id: "A-11", occupied: false, isDisable: false, type: "car" },
+    { id: "A-12", occupied: true, isDisable: false, type: "car" },
+  ],
+  "2": [
+    // Left side
+    { id: "A-01", occupied: false, isDisable: true, type: "car" },
+    { id: "A-02", occupied: false, isDisable: true, type: "car" },
+    { id: "A-03", occupied: false, isDisable: false, type: "car" },
+    { id: "A-04", occupied: true, isDisable: false, type: "car" },
+    { id: "A-05", occupied: true, isDisable: false, type: "car" },
+
+    // Right side, after left side spots
+    { id: "A-07", occupied: false, isDisable: true, type: "car" },
+    { id: "A-08", occupied: false, isDisable: true, type: "car" },
+    { id: "A-09", occupied: true, isDisable: false, type: "car" },
+    { id: "A-10", occupied: true, isDisable: false, type: "car" },
+    { id: "A-11", occupied: false, isDisable: false, type: "car" },
+  ],
+};
+
+const ParkingGrid = ({ floor }) => {
+  // Split the parking spots into two groups
+  const floorData = parkingSpots[floor] || [];
+  const splitIndex = Math.ceil(floorData.length / 2);
+  const leftSideSpots = floorData.slice(0, splitIndex);
+  const rightSideSpots = floorData.slice(splitIndex);
+
+  const renderParkingSpots = (spots, side) => {
+    return spots.map((spot, index) => (
+      <View style={styles.parkingSpot} key={`spot-${side}-${index}`}>
+        {spot.occupied ? (
+          <Image
+            source={carIcon}
+            style={styles.occupiedSpot}
+            resizeMode="contain"
+          />
+        ) : spot.isDisable ? (
+          // Show a different icon or style if the spot is disabled
+          <Image
+            source={disabilityIcon}
+            style={styles.disabledSpot}
+            resizeMode="contain"
+          />
+        ) : (
+          <Text style={styles.spotNumber}>{spot.id}</Text>
+        )}
+      </View>
+    ));
+  };
+
+  return (
+    <View style={styles.gridContainer}>
+      <View style={styles.column}>
+        {renderParkingSpots(leftSideSpots, "left")}
+      </View>
+      <View style={styles.middleSpace}>
+        <Icon name="arrow-downward" size={30} color="#000" />
+      </View>
+      <View style={styles.column}>
+        {renderParkingSpots(rightSideSpots, "right")}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridContainer: {
+    flexDirection: "row",
+    //flex: 1, // The grid container will take the full width of its parent
+    justifyContent: "space-between", // This will place the space evenly between the two columns
+    paddingHorizontal: 10, // Add some horizontal padding if needed
+  },
+  column: {
+    flexDirection: "row", // Direction of children within the column
+    flexWrap: "wrap", // Wrap children to form a grid
+    width: "45%", // Each column takes up 45% of the grid container width
+    justifyContent: "space-between", // Spacing between inner items
+  },
+  middleSpace: {
+    width: "10%", // The middle space takes up 10% of the grid container width
+    paddingTop: "30%",
+  },
+  parkingSpot: {
+    width: "50%", // Each spot takes up half of its parent (column) width, creating two columns
+    aspectRatio: 1, // Keep the spots square
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5, // Padding inside each parking spot
+    marginBottom: 5, // Space between rows
+  },
+  occupiedSpot: {
+    width: "80%",
+    height: "80%",
+  },
+  spotNumber: {},
+  disabledSpot: {
+    width: "60%",
+    height: "60%",
+  },
+});
+
+export default ParkingGrid;
